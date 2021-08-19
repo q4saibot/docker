15fc9614e2eb6752aa1e865bd53d7dffa075d10a
Q4.SaiBot <Q4.SaiBot@GMail.com>
2021-08-19 06:22:21 +0300

    UIkit v5

---
+++ images/uikit/build.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+docker	builder build \
+		--tag uikit:3.7.2-apache \
+		.
---
+++ images/uikit/run.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+docker run \
+  --name UIkit \
+  --hostname uikit \
+  --publish 80:80 \
+  --volume uikit_html:/usr/local/apache2/htdocs \
+  --detach \
+  uikit:3.7.2
+  
